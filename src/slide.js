71926c788701ae97e98c48530d14e90b839aee54
Bohdan Sichkaruk <82411127+Sichkaruk@users.noreply.github.com>
2021-07-23 12:12:31 +0300

    slider

---
+++ src/slide.js
@@ -0,0 +1,25 @@
+  
+$(function () {
+    $('.slider').slick({
+        arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow:1  
+				}
+			}
+		]
+	});
+});
