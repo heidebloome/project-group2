bfca3219a7eb355bc6764646ccf2f896bc5b1b21
Vira Velmozhna <viravelmozhna@gmail.com>
2021-07-29 10:00:49 +0300

    modal window fix

--- src/slide.js
+++ src/slide.js
@@ -1,48 +1,47 @@
-  
 $(function () {
-    $('.slider').slick({
-        arrows:false,
-		dots:true,
-		slidesToShow:1,
-		autoplay:false,
-		speed:1000,
-		autoplaySpeed:800,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow:1  
-				}
-			}
-		]
-	});
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: false,
+    speed: 1000,
+    autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
 
-var e = document.getElementById("watchme");
-e.addEventListener("animationstart", listener, false);
-e.addEventListener("animationend", listener, false);
-e.addEventListener("animationiteration", listener, false);
+var e = document.getElementById('watchme');
+e.addEventListener('animationstart', listener, false);
+e.addEventListener('animationend', listener, false);
+e.addEventListener('animationiteration', listener, false);
 
-e.className = "slidein";
+e.className = 'slidein';
 
 function listener(e) {
-  var l = document.createElement("li");
-  switch(e.type) {
-    case "animationstart":
-      l.innerHTML = "Started: elapsed time is " + e.elapsedTime;
+  var l = document.createElement('li');
+  switch (e.type) {
+    case 'animationstart':
+      l.innerHTML = 'Started: elapsed time is ' + e.elapsedTime;
       break;
-    case "animationend":
-      l.innerHTML = "Ended: elapsed time is " + e.elapsedTime;
+    case 'animationend':
+      l.innerHTML = 'Ended: elapsed time is ' + e.elapsedTime;
       break;
-    case "animationiteration":
-      l.innerHTML = "New loop started at time " + e.elapsedTime;
+    case 'animationiteration':
+      l.innerHTML = 'New loop started at time ' + e.elapsedTime;
       break;
   }
-  document.getElementById("output").appendChild(l);
+  document.getElementById('output').appendChild(l);
 }
